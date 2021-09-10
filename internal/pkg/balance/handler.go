865807f1a58ede653aba2e602c8148d4fbc0d6c8
Suvorov Nikita <suvnik.2020@gmail.com>
2021-09-10 23:28:04 +0300

    [Fix] Mocks

--- internal/pkg/balance/handler.go
+++ internal/pkg/balance/handler.go
@@ -2,8 +2,6 @@ package balance
 
 import "net/http"
 
-//go:generate mockgen -destination=./mock/mock_handler.go -package=mock github.com/avito-test-case/internal/pkg/balance Handler
-
 type Handler interface {
 	GetUserBalance(w http.ResponseWriter, r *http.Request)
 	WithdrawUserBalance(w http.ResponseWriter, r *http.Request)
--- internal/pkg/transaction/handler.go
+++ internal/pkg/transaction/handler.go
@@ -2,8 +2,6 @@ package transaction
 
 import "net/http"
 
-//go:generate mockgen -destination=./mock/mock_handler.go -package=mock github.com/avito-test-case/internal/pkg/transaction Handler
-
 type Handler interface {
 	GetUserTransactions(w http.ResponseWriter, r *http.Request)
 }
