e8ddff0931f32c96a9dd1d0c300f3e77b1187028
Suvorov Nikita <suvnik.2020@gmail.com>
2021-09-10 19:32:28 +0300

    [Add] Pkg models

--- internal/pkg/models/balance.go
+++ internal/pkg/models/balance.go
@@ -1,1 +1,28 @@
 package models
+
+type UserBalance struct {
+	Id       uint64  `json:"-"`
+	Balance  float64 `json:"balance"`
+	Currency string  `json:"currency"`
+}
+
+type ImproveBalance struct {
+	Id    uint64  `json:"id"`
+	Value float64 `json:"value"`
+}
+
+type WithdrawBalance struct {
+	Id    uint64  `json:"id"`
+	Value float64 `json:"value"`
+}
+
+type Transfer struct {
+	SenderId   uint64  `json:"sender_id"`
+	ReceiverId uint64  `json:"receiver_id"`
+	Value      float64 `json:"value"`
+}
+
+type TransferResult struct {
+	Receiver UserBalance `json:"receiver"`
+	Sender   UserBalance `json:"sender"`
+}
